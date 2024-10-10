268f5c8a7a7f71a20bba9afedb08ac41362d440e
deunoh <denovann@live.fr>
2024-10-10 17:08:15 +0200

    composant mark commencé

---
+++ src/components/Space/Mark/Mark.jsx
@@ -0,0 +1,24 @@
+import './Mark.scss';
+
+const Mark = () => {
+  const url = 'https://music.apple.com/';
+  const faviconUrl = `https://www.google.com/s2/favicons?domain=${url}&sz=64`;
+
+  return (
+    <a
+      role="button"
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      title="apple music"
+      className="Mark"
+    >
+      <div className="TileImgContainer">
+        <img src={faviconUrl} alt="Apple Music icon" />
+      </div>
+      <p className="TileTitle">Apple music avec un titre long</p>
+    </a>
+  );
+};
+
+export default Mark;
--- src/components/Space/Space.jsx
+++ src/components/Space/Space.jsx
@@ -1,7 +1,13 @@
+import Mark from './Mark/Mark';
 import './Space.scss';
 
 const Space = () => {
-  return <div className="space-container">Ici les marks</div>;
+  return (
+    <div className="space-container">
+      <Mark />
+      <Mark />
+    </div>
+  );
 };
 
 export default Space;
