2f6b3c7eb126293b55b124012b4981bb7143eb32
deunoh <denovann@live.fr>
2024-11-01 14:17:44 +0100

    Footer ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,23 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <ul>
-        <Link to="/privacy-policy">politique de confidentialité</Link>
+        <Link to="/privacy-policy" className="footer-link">
+          Politique de confidentialité
+        </Link>
         <p>
-          Développé par <a href="https://denovann.fr/">Denovann</a>
+          Développé par{' '}
+          <a
+            href="https://denovann.fr/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            Denovann
+          </a>
         </p>
-        <Link to="/mentions-legales">Mentions légales</Link>
+        <Link to="/mentions-legales" className="footer-link">
+          Mentions légales
+        </Link>
       </ul>
     </footer>
   );
