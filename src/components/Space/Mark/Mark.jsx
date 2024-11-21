7f3f24d70018b9d9e629c472fba607923f250d04
deunoh <denovann@live.fr>
2024-11-21 15:28:44 +0100

    add favion exception for use google api

--- src/components/Space/Mark/Mark.jsx
+++ src/components/Space/Mark/Mark.jsx
@@ -7,23 +7,34 @@ import { FiEdit2 } from 'react-icons/fi';
 import './Mark.scss';
 import { deleteMark, toggleEditMarkModal } from '../../../actions/markActions';
 import DescriptionModal from '../../Modals/DescriptionModal/DescriptionModal';
-import defaultIcon from '../../../assets/default-mark-icon.png';
 
 const Mark = ({ id, url, name, description = null }) => {
+  const isEditMode = useSelector((state) => state.mark.isEditMode);
+  const spaceId = useSelector((state) => state.mark.spaceSelected.id);
   const [showDescriptionModal, setShowDescriptionModal] = useState(false);
   const dispatch = useDispatch();
+
+  // get favicon
   const domain = new URL(url).hostname;
+  const FAVICON_EXCEPTIONS = [
+    'github.com',
+    'github.io',
+    'youtube.com',
+    'www.youtube.com',
+  ];
+
   // With google
-  // const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${url}`;
-  // With faviconextractor
-  const faviconUrl = `https://www.faviconextractor.com/favicon/${domain}?larger=true`;
+  // const faviconUrl = `https://www.google.com/s2/favicons?domain=${domain}&sz=64`;
+  // With faviconextractor (best)
+  // const faviconUrl = `https://www.faviconextractor.com/favicon/${domain}?larger=true`;
   // With icon horse (limited request)
   // const faviconUrl = `https://icon.horse/icon/${domain}`;
   // With duckduckgo (poor quality)
   // const faviconUrl = `https://icons.duckduckgo.com/ip3/${domain}.ico`;
+  const faviconUrl = FAVICON_EXCEPTIONS.includes(domain)
+    ? `https://www.google.com/s2/favicons?domain=${domain}&sz=64`
+    : `https://www.faviconextractor.com/favicon/${domain}?larger=true`;
 
-  const isEditMode = useSelector((state) => state.mark.isEditMode);
-  const spaceId = useSelector((state) => state.mark.spaceSelected.id);
   const handleRemove = () => {
     dispatch(deleteMark(id, spaceId));
   };
