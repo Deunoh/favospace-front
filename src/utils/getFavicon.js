aff8d38523722f2f076b2e3c07c3643043900b2e
deunoh <denovann@live.fr>
2025-02-19 23:34:06 +0100

    icon horse test

--- src/utils/getFavicon.js
+++ src/utils/getFavicon.js
@@ -15,11 +15,18 @@ const FAVICON_EXCEPTIONS = [
 // With duckduckgo (poor quality)
 // const faviconUrl = `https://icons.duckduckgo.com/ip3/${domain}.ico`;
 
+// const getFavicon = (url) => {
+//   const domain = new URL(url).hostname;
+//   return FAVICON_EXCEPTIONS.includes(domain)
+//     ? `https://www.google.com/s2/favicons?domain=${domain}&sz=64`
+//     : `https://www.faviconextractor.com/favicon/${domain}?larger=true`;
+// };
+
 const getFavicon = (url) => {
   const domain = new URL(url).hostname;
   return FAVICON_EXCEPTIONS.includes(domain)
     ? `https://www.google.com/s2/favicons?domain=${domain}&sz=64`
-    : `https://www.faviconextractor.com/favicon/${domain}?larger=true`;
+    : `https://icon.horse/icon/${domain}`;
 };
 
 export default getFavicon;
